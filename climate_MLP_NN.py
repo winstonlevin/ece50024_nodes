ddd3c926a1c8839b16257ae147b3113a95aa0a2f
Alvin Ye <alvinye9@gmail.com>
2024-04-18 23:15:26 -0400

    fixed some more formatting issues

--- climate_MLP_NN.py
+++ climate_MLP_NN.py
@@ -102,7 +102,7 @@ plt.plot(epochs_range, val_acc, label='Validation Accuracy')
 plt.legend(loc='lower right')
 plt.title(f'Training and Validation Accuracy, H={H}')
 
-plt.figtext(0.4, 0.05, f'Test Accuracy: {test_accuracy:.2f}', ha='center', fontsize=30)
+plt.figtext(0.5, 0.025, f'Test Accuracy: {test_accuracy:.2f}', ha='center', fontsize=30)
 
 plt.subplot(1, 2, 2)
 plt.plot(epochs_range, loss, label='Training Loss')
